306ff036a843b00b2703d45b0cf5e236a122d5d4
DARKsarin <ty82afg12@gmail.com>
2023-03-17 08:38:24 +0900

    valueType

---
+++ src/main/ifAndFor.java
@@ -0,0 +1,15 @@
+package main;
+
+public class ifAndFor {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 0;
+
+        if(x == 1) {
+            System.out.println("x is true");
+        }
+        if(y == 1) {
+            System.out.println("y is true");
+        }
+    }
+}
---
+++ src/main/valueType.java
@@ -0,0 +1,15 @@
+package main;
+
+public class valueType {
+    public static void main(String[] args) {
+        int a = 0b0101; // 2진수
+        int b = 0206; // 8진수
+        int c = 365; // 10진수
+        int d = 0x12; // 16진수
+
+        System.out.println("a : " + a);
+        System.out.println("b : " + b);
+        System.out.println("c : " + c);
+        System.out.println("d : " + d);
+    }
+}
