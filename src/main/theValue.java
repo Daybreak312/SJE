b33ae2f6494807bd6cd2d6d6574d84edb8d8d5ea
DARKsarin <ty82afg12@gmail.com>
2023-03-17 20:05:14 +0900

    theVariable

--- src/main/theValue.java
+++ src/main/theValue.java
@@ -2,6 +2,26 @@ package main;
 
 public class theValue {
     public static void main(String[] args) {
+        byte valByte = 127;
+        short valShort = 32767;
+        char valChar = 'A';
+        int valInt = 2147483647;
+        long valLong = 9223372036854775807L;
 
+        System.out.println("valByte : " + valByte);
+        System.out.println("valShort : " + valShort);
+        System.out.println("valChar : " + valChar);
+        System.out.println("valInt : " + valInt);
+        System.out.println("valLong : " + valLong);
+
+        int val1 = 0b1011;
+        int val2 = 0206;
+        int val3 = 365;
+        int val4 = 0xB3;
+
+        System.out.println("val1 : " + val1);
+        System.out.println("val2 : " + val2);
+        System.out.println("val3 : " + val3);
+        System.out.println("val4 : " + val4);
     }
 }
