279ce05e94857b77dcde5c683684a97871288b08
DARKsarin <ty82afg12@gmail.com>
2023-03-28 12:45:26 +0900

    my code

---
+++ src/main/backjoonN2480.java
@@ -0,0 +1,22 @@
+package main;
+
+import java.util.*;
+
+public class backjoonN2480 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d1 = sc.nextInt(), d2 = sc.nextInt(), d3 = sc.nextInt();
+        if (d1 == d2 && d1 == d3)
+            System.out.println(10000 + d1 * 1000);
+        else if (d1 == d2 || d1 == d3)
+            System.out.println(1000 + d1 * 100);
+        else if (d2 == d3)
+            System.out.println(1000 + d2 * 100);
+        else if (d1 > d2 && d1 > d3)
+            System.out.println(d1 * 100);
+        else if (d2 > d1 && d2 > d3)
+            System.out.println(d2 * 100);
+        else
+            System.out.println(d3 * 100);
+    }
+}
