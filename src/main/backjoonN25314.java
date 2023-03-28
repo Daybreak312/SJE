e2f6c8b0184457b0c4ea376b86434b7186a9addf
DARKsarin <ty82afg12@gmail.com>
2023-03-28 19:39:31 +0900

    my code

---
+++ src/main/backjoonN25314.java
@@ -0,0 +1,19 @@
+/* Backjoon n.25314
+N바이트 크기를 만들기 위해 int 앞에 long을 붘여 만들려하는 친구가 있다.
+이 친구를 위해 대신해서 프로그램을 짜주자. */
+
+package main;
+
+import java.util.*;
+
+public class backjoonN25314 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i, a = sc.nextInt();
+        a /= 4;
+        for (i = 0; i < a; i++) {
+            System.out.print("long ");
+        }
+        System.out.print("int");
+    }
+}
